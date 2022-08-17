9b93f02f4a1ae4044923526e6eaab67579ad1fcd
castlewong <swy0114@qq.com>
2022-08-17 23:26:08 +0800

    3rd Commit

--- js/maintry.js
+++ js/maintry.js
@@ -1,20 +1,41 @@
 
-// 我他喵的还就发现了, 我动不动就是因为没有el上而出不来效果
+// Every detail counts, since if it doesn't, it won't work.
 var app = new Vue({
     el: "#player",
     data: {
         query:"",
-        musicList:[]
+
+        musicList:[],
+
+        musicUrl: "",
+
+        isPlaying: false
     },
     methods: {
         searchMusic: function(){
             // this has changed
             var that = this;
-            axios.get("https://autumnfish.cn/search?keywords="+this.query)
-            .then(function(response){
+            axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(
+                function(response){
                 // console.log(response);
                 that.musicList = response.data.result.songs;
-            },function(err){})
+                console.log(response.data.result.songs);
+            },function(err) {});
+        },
+        playMusic: function(musicId) {
+            var that = this;
+            axios.get("https://autumnfish.cn/song/url?id=" + musicId)
+            .then(function(response) {
+                that.musicUrl = response.data.data[0].url;
+            },
+            function(err) {}
+            );
+        },
+        play: function () {
+            this.isPlaying = true;
+        },
+        pause: function () {
+            this.isPlaying = false;
         }
     },
-})
+});
